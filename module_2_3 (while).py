8fb351187b44b69cd8f895e3433b22033a0f636a
Aleksei-Strel <an@strelnikov-mail.ru>
2024-08-06 19:16:43 +0400

    Add files via upload

---
+++ module_2_3 (while).py
@@ -0,0 +1,10 @@
+my_spase = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_spase):
+    if my_spase[a] > 0:
+        print(my_spase[a])
+    a += 1
+    # continue
+    # if my_spase[a] < 0:
+    #      break
+    # a += 1
