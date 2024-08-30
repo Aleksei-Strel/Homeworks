69189d6a26f7894f03274d21b32f2eac564065be
Aleksei-Strel <an@strelnikov-mail.ru>
2024-08-30 12:49:16 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,24 +1,20 @@
-# создаём функцию send_email
-def send_email(message,recipient,sender = "univerity.help@gmail.com"):
-    if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
-          # создаём  1 условие
-        print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
+def send_email(message, recipient, sender="univerity.help@gmail.com"):
+    if '@' not in recipient or not recipient.endswith(
+            ('.com', '.ru', '.net')) or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif recipient == sender:
-        # создём 2 условие
         print("Нельзя отправить письмо самому себе!")
-        # создаём 3 условие
-    elif sender == "university.help@gmail.com":
+    elif sender == "univerity.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
-        # создаём 4 условие
-    elif sender !="university.help@gmail.com":
+    elif sender != "university.help@gmail.com":
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
 
-send_email('messagje','university.help@mail.com','@.com')
-send_email('messagje','university.help@mail.com')
-send_email('messagje','universi@.com')
-send_email('messagje','universi@.co')
-send_email('messagje','universi@.com')
+send_email('messagje','university.@helpail.cm', sender = '@.com')
+send_email('messagje','university.@helpail.com', sender = '.com')
+send_email('messagje','mail@mail.ru',    'mail@mail.ru')
+send_email('messagje','university@.com')
+send_email('messagje','university@.com',    'mail@mail.ru')
 
 
